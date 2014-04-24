0335262fde9ac83344140dfba3fc64548dc37a56
Eino Juhani Oltedal <e.j.oltedal@fys.uio.no>
2014-04-24 22:33:43 +0200

    fixed 2009 round 1C problemB started problemC

--- 2009/round1C/problemB/problemB.py
+++ 2009/round1C/problemB/problemB.py
@@ -26,22 +26,43 @@ class Firefly ():
     def getZ(self):
         return self.z
 
-def centerOfGravity(flies):
+    def getVx(self):
+        return self.vx
+
+    def getVy(self):
+        return self.vy
+
+    def getVz(self):
+        return self.vz
+
+def centerOfGravityandMotion(flies):
     point = []
     xc = 0.0
     yc = 0.0
     zc = 0.0
+    vxc = 0.0
+    vyc = 0.0
+    vzc = 0.0
     N = len(flies)
     for fly in flies:
         xc += fly.getX()
         yc += fly.getY()
         zc += fly.getZ()
+        vxc += fly.getVx()
+        vyc += fly.getVy()
+        vzc += fly.getVz()
     xc = xc/N
     yc = yc/N
     zc = zc/N
+    vxc = vxc/N
+    vyc = vyc/N
+    vzc = vzc/N
     point.append(xc)
     point.append(yc)
     point.append(zc)
+    point.append(vxc)
+    point.append(vyc)
+    point.append(vzc)
     return point
 
 
@@ -53,7 +74,7 @@ time = 0
 is_new_case = True
 num_fireflies = 0
 fireflies = []
-num = 1
+num = 0
 for line in stdin:
     if is_new_case:
         num_fireflies = int(line)
@@ -66,15 +87,21 @@ for line in stdin:
         fireflies.append(Firefly(case_cnt,int(line.split()[0]),int(line.split()[1]),int(line.split()[2]),int(line.split()[3]),int(line.split()[4]),int(line.split()[5])))
         case_cnt += 1
         if (case_cnt == num_fireflies):
-            prev_center = 15001
-            center = 15000
-            while (center <= prev_center and time <= 100):
-                prev_center = center
-                point = centerOfGravity(fireflies)
+            point = centerOfGravityandMotion(fireflies)
+            #check for 0 division
+            if (point[3]**2+point[4]**2+point[5]**2 < 1e-9):
+                #t = 0 is min
+                t = 0
                 center = sqrt(point[0]**2 + point[1]**2 + point[2]**2)
-                for fly in fireflies:
-                    fly.timestep()
-                time += 1
+            else:
+                t = (point[0]*point[3] +point[1]*point[4] + point[2]*point[5])/(-point[3]**2-point[4]**2-point[5]**2)
+                if (t < 0):
+                    t = 0
+                    center = sqrt(point[0]**2 + point[1]**2 + point[2]**2)
+                else:
+                    center = sqrt((point[0]+point[3]*t)**2 + (point[1]+point[4]*t)**2 + (point[2]+point[5]*t)**2)
             is_new_case = True
+            num += 1
             # python3: print("Case #" + str(num) + ": " + str(prev_center) + " " + str(time-2))
-            print "Case #%d: %.8f %.8f" % (num, prev_center, time-2)
+            #print "Case #%d: %.8f %.8f" % (num, prev_center, time-2)
+            print("Case #{}: {:.8f} {:.8f}".format(num,center,t))
--- 2009/round1C/problemC/problemC.py
+++ 2009/round1C/problemC/problemC.py
@@ -1,1 +1,41 @@
+from sys import stdin
 #Bribe the Prisoners
+
+def release(prison, tbr):
+    middle = round(prison/2)
+    tbrA = []
+    tbrB = []
+    smallest = prison
+    for t in tbr:
+        if (abs(middle-t) <= smallest):
+            smallest = abs(middle-t)
+            split = t
+    a = split - 1
+    b = prison  - split 
+    for t in tbr:
+        if (t < a+1):
+            tbrA.append(t)
+        elif (t > a+1):
+            tbrB.append(t-(a+1))
+   # a = p[0:int(smallest)-1] #lower array
+   # b = prison[int(smallest):] #upper array
+    coins = prison - 1
+    if ((a > 0) and (len(tbrA) > 0)):
+        coins += release(a, tbrA)
+    if ((b > 0) and (len(tbrB) > 0)):
+        coins += release(b, tbrB)
+    return coins
+
+
+
+num_cases = int(stdin.readline())
+
+for i in range(num_cases):
+    pq = stdin.readline().split()
+    p = int(pq[0])
+    q = int(pq[1])
+    tbr = stdin.readline().split() #to be released
+    tbr = [int(i) for i in tbr]
+    coins = release(p, tbr)
+    print("Case #" + str(i+1) +": " + str(coins))
+
